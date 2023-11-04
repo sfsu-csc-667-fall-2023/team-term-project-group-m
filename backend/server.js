0201edf27f2ee2f23d0bfc7582a193c2d9ad8ff1
Matthew Marcos <112049111+mmarcos3@users.noreply.github.com>
2023-11-03 23:15:28 -0700

    testing with server.js now

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
-// const path = require("path");
-// const createError = require("http-errors");
-// const cookieParser = require("cookie-parser");
-// const morgan = require("morgan");
+const path = require("path");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 const express = require("express");
 const app = express();
@@ -14,22 +14,22 @@ app.use("/test", testRoutes);
 // Commenting out to test db connection
 
 
-// app.use(morgan("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
-// app.use(express.static(path.join(__dirname, "static")));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "static")));
 
-// const rootRoutes = require("./routes/root");
-// app.use("/", rootRoutes);
-// const PORT = process.env.PORT || 3000;
+const rootRoutes = require("./routes/root");
+app.use("/", rootRoutes);
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
-// app.use((request, response, next) => {
-//   next(createError(404));
-// });
+app.use((request, response, next) => {
+  next(createError(404));
+});
