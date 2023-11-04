4003df8bd123aff17b23ae4c86cec09664eea765
Matthew Marcos <112049111+mmarcos3@users.noreply.github.com>
2023-11-03 22:40:32 -0700

    commented out some server.js to test db connection

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,26 @@ const app = express();
 
 const testRoutes = require("./routes/test/index.js");
 app.use("/test", testRoutes);
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "static")));
 
-const rootRoutes = require("./routes/root");
-app.use("/", rootRoutes);
-const PORT = process.env.PORT || 3000;
+// Commenting out to test db connection
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
 
-app.use((request, response, next) => {
-  next(createError(404));
-});
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "static")));
+
+// const rootRoutes = require("./routes/root");
+// app.use("/", rootRoutes);
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+// app.use((request, response, next) => {
+//   next(createError(404));
+// });
