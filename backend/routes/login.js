5660bfac75b235d06b19f7cf039f502657087942
Matthew Marcos <112049111+mmarcos3@users.noreply.github.com>
2023-12-09 14:46:40 -0800

    io require in login

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require("../db/connection.js");
 const bcrypt = require('bcrypt');
+const io = require('socket.io')(server);
 
 router.post("/login", async (request, response) => {
     var email = request.body.email;
